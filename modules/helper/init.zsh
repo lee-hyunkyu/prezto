b20839e57c1e7c8155b381a971565e96b343e929
Hyunkyu Lee <hkl0902@gmail.com>
2024-11-13 22:04:12 -0800

    fix prompt with branch

--- modules/helper/init.zsh
+++ modules/helper/init.zsh
@@ -5,6 +5,46 @@
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
 #
 
+## Git Functions
+
+function __git_prompt_git() {
+  GIT_OPTIONAL_LOCKS=0 command git "$@"
+}
+
+function git_current_branch() {
+  local ref
+  ref=$(__git_prompt_git symbolic-ref --quiet HEAD 2> /dev/null)
+  local ret=$?
+  if [[ $ret != 0 ]]; then
+    [[ $ret == 128 ]] && return  # no git repo.
+    ref=$(__git_prompt_git rev-parse --short HEAD 2> /dev/null) || return
+  fi
+  echo ${ref#refs/heads/}
+}
+
+function git_prompt_info() {
+  # If we are on a folder not tracked by git, get out.
+  # Otherwise, check for hide-info at global and local repository level
+  if ! __git_prompt_git rev-parse --git-dir &> /dev/null; then
+    return 0
+  fi
+
+  local ref
+  ref=$(__git_prompt_git symbolic-ref --short HEAD 2> /dev/null) \
+  || ref=$(__git_prompt_git rev-parse --short HEAD 2> /dev/null) \
+  || return 0
+
+  # # Use global ZSH_THEME_GIT_SHOW_UPSTREAM=1 for including upstream remote info
+  # local upstream
+  # if (( ${+ZSH_THEME_GIT_SHOW_UPSTREAM} )); then
+  #   upstream=$(__git_prompt_git rev-parse --abbrev-ref --symbolic-full-name "@{upstream}" 2>/dev/null) \
+  #   && upstream=" -> ${upstream}"
+  # fi
+
+  # echo "${ZSH_THEME_GIT_PROMPT_PREFIX}${ref:gs/%/%%}${upstream:gs/%/%%}$(parse_git_dirty)${ZSH_THEME_GIT_PROMPT_SUFFIX}"
+  echo "%F{1}[$(git_current_branch)]"
+}
+
 # Checks if a file can be autoloaded by trying to load it in a subshell.
 function is-autoloadable {
   ( unfunction $1 ; autoload -U +X $1 ) &> /dev/null
